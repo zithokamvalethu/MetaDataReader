7aa9bcd16399617ee5b669e849c9d7deb257531e
zithokamvalethu <kamvalethu.zitho1705@gmail.com>
2022-01-19 10:58:40 +0200

    added label tags

--- file-reader/src/App.js
+++ file-reader/src/App.js
@@ -16,7 +16,6 @@ function App() {
       type: file.type,
       size: file.size,
       blob: file.type,
-      
     });
     console.log(file);
   };
@@ -26,14 +25,19 @@ function App() {
         <label>Attach File</label>
         <input id="file" type="file" multiple onChange={handleFileInput} />
         <br />
-        <label>File name
-        </label><br/><p>{markedFile.name} </p>
+        <label>File name</label>
+        
+        {markedFile.name} <p></p>
         <br />
-        File Type <p>{markedFile.type} </p>
+        <label>File Type</label>
+        {markedFile.type} <p></p>
         <br />
-        File Size:<p>{markedFile.size} </p>
+        <label>File Size</label>
+        {markedFile.size}
+        <p> </p>
         <br />
-        Blob <p>{markedFile.blob} </p>
+        <label>File Blob</label>
+        {markedFile.blob} <p> </p>
       </div>
     </form>
   );
